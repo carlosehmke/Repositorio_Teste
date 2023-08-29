830f216176da41387ac6306db62ccb08d3526f74
carlosehmke <carlos.ehmke.agra@gmail.com>
2023-08-28 21:01:22 -0300

    Soma de dois numeros aleatórios

--- teste/Tete_github.py
+++ teste/Tete_github.py
@@ -1,8 +1,11 @@
 import random
 
 print('Olá Mundo"')
-nome = input('Qual é o seu nome?')
-print('Seu nome é ',nome)
+#nome = input('Qual é o seu nome?')
+#print('Seu nome é ',nome)
 
-numero = random.randint(0,1000)
-print(numero)
+numero1 = random.randint(0,1000)
+print(numero1)
+numero2 = random.randint(0,1000)
+print(numero2)
+print(numero1+numero2)
